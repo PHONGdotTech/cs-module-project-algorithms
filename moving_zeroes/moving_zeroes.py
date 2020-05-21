f850f2ad8b2acf693b84076fdcf96bd3e5a9022b
Phong Yang <phongyang88@hotmail.com>
2020-05-21 00:48:06 -0500

    added recursive method to move zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,23 +4,48 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-    for i in range(len(arr)-1):
-        if arr[i] == 0:
-            arr.append(arr.pop(i))
+    count = 0
+    for x in arr:
+        if x == 0:
+            count += 1
 
-    for i in range(len(arr)-1):
-        if arr[i] == 0:
-            arr.append(arr.pop(i))
+        if count == len(arr):
+            return arr
 
-    for i in range(len(arr)-1):
-        if arr[i] == 0:
-            arr.append(arr.pop(i))
+    if arr[0] == 0:
+        for i in range(len(arr)-1):
+            if arr[i] == 0:
+                arr.pop(i)
+                arr.append(0)
+        return moving_zeroes(arr)
+    else:
+        for i in range(len(arr)-1):
+            if arr[i] == 0:
+                arr.pop(i)
+                arr.append(0)
 
     return arr
+
+    # print(arr)
+    # for i in range(len(arr)-1):
+    #     if arr[i] == 0:
+    #         arr.append(arr.pop(i))
+
+    # print(arr)
+    # for i in range(len(arr)-1):
+    #     if arr[i] == 0:
+    #         arr.append(arr.pop(i))
+
+    # print(arr)
+    # for i in range(len(arr)-1):
+    #     if arr[i] == 0:
+    #         arr.append(arr.pop(i))
+
+    # return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     # arr = [0, 3, 1, 0, -2]
-    arr = [0, 0, 0, 0, 3, 2, 1] 
+    arr = [0, 0, 0, 0, 0, 0, 3, 0,0,0,0,2,0,0,0, 1] 
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
